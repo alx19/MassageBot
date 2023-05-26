d5af4569544668e0931cf812688658fa43f1f463
Aleksandr Sukharev <a.sukharev@rambler-co.ru>
2023-05-26 13:03:38 +0300

    logging messages in another file

--- client/client.rb
+++ client/client.rb
@@ -24,8 +24,8 @@ class Client
         send_message(chat_id: @chat_id, text: 'Извините, данный слот уже занят :(')
       else
         user = MongoClient.user_info(@chat_id)
-        MongoClient.reserve_via_date_time(date: slot['date'], time: slot['time'], link: "<a href=\"tg://user?id=#{user['id']}\">#{user['name']}</a>", id: @chat_id)
-        Calendar.add_event_to_calendar(slot['unix_timestamp'], "Массаж #{user['name']}", "t.me/#{user['username']}")
+        unix_timestamp = MongoClient.reserve_via_date_time(date: slot['date'], time: slot['time'], link: "<a href=\"tg://user?id=#{user['id']}\">#{user['name']}</a>", id: @chat_id)
+        Calendar.add_event_to_calendar(unix_timestamp, "Массаж #{user['name']}", "t.me/#{user['username']}")
         send_message(chat_id: @chat_id, text: 'Спасибо за запись! За день до массажа мы на помним вам о нем. Ждем вас на массаж :)')
         send_message(chat_id: MASTER_ID, text: "<a href=\"tg://user?id=#{user['id']}\">#{user['name']}</a> записался на массаж #{russian_date}", parse_mode: 'HTML')
       end
@@ -34,10 +34,11 @@ class Client
       slots = MongoClient.active_slots
       if slots != []
         kb = slots.map { |s| [Telegram::Bot::Types::KeyboardButton.new(text: "Записаться на #{s['russian_datetime']}")] }
+        kb << [Telegram::Bot::Types::KeyboardButton.new(text: 'Назад')]
         markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb, one_time_keyboard: true)
-        @bot.api.send_message(chat_id: @chat_id, text: 'Выберите слот для записи. На слоты до 17-00 действует скидка в 10%', reply_markup: markup)
+        send_message(chat_id: @chat_id, text: 'Выберите слот для записи. На слоты до 17-00 действует скидка в 10%', reply_markup: markup)
       else
-        @bot.api.send_message(chat_id: @chat_id, text: 'Свободных слотов нет :(')
+        send_message(chat_id: @chat_id, text: 'Свободных слотов нет :(')
       end
     when 'Схема проезда'
       send_path
@@ -45,17 +46,19 @@ class Client
     when 'Противопоказания'
       send_contraindications
       show_options
+    when 'Назад'
+      show_options
     when 'Отменить запись'
-      @bot.api.send_message(chat_id: @chat_id, text: 'Для отмены записи напишите мастеру @alicekoala')
+      send_message(chat_id: @chat_id, text: 'Для отмены записи напишите мастеру @alicekoala')
       show_options
     when '/start'
       greetings
     else
       if not_registred?
         MongoClient.add_user(id: @chat_id, name: @message.text.strip, username: @message.from.username)
-        @bot.api.send_message(chat_id: @chat_id, text: "Спасибо за регистрацию, #{@message.text.strip}")
+        send_message(chat_id: @chat_id, text: "Спасибо за регистрацию, #{@message.text.strip}")
       else
-        @bot.api.send_message(chat_id: @chat_id, text: 'Нет такой команды, пожалуйста выберите из команд ниже')
+        send_message(chat_id: @chat_id, text: 'Нет такой команды, пожалуйста выберите из команд ниже')
       end
       show_options
     end
@@ -63,9 +66,17 @@ class Client
 
   private
 
+  def send_message(**data)
+    begin
+      @bot.api.send_message(**data)
+    rescue => e
+      MyLogger.new('messages_log.txt').log(e)
+    end
+  end
+
   def show_options
     kb = OPTIONS.map { |o| [Telegram::Bot::Types::KeyboardButton.new(text: o)] }
     markup = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: kb, one_time_keyboard: true)
-    @bot.api.send_message(chat_id: @chat_id, text: 'Что вы хотите сделать?', reply_markup: markup)
+    send_message(chat_id: @chat_id, text: 'Что вы хотите сделать?', reply_markup: markup)
   end
 end
--- my_logger.rb
+++ my_logger.rb
@@ -1,5 +1,9 @@
 class MyLogger
+  def initialize(file = 'log.txt')
+    @file = file
+  end
+
   def log(error, message_text = '')
-    File.open('log.txt', 'a') { |f| f.write "#{Time.now}: #{error}, message_text: #{message_text}\n" }
+    File.open(@file, 'a') { |f| f.write "#{Time.now}: #{error}, message_text: #{message_text}\n" }
   end
 end

3a4fe6a90fb749372201df2a0ce21cd5970d69be
Alx19 <alx19sucharev@gmail.com>
2023-07-13 14:08:14 +0300

    remove pics script

--- pictures/pics.rb
+++
@@ -1,9 +0,0 @@
-require 'telegram/bot'
-require 'base64'
-require 'faraday'
-require 'faraday/multipart'
-TOKEN = '5991437527:AAF7Fcr9eNQ6eykX6SVqrFvcVH7qC5_Q0rY'
-
-10.times do |i|
-  puts Telegram::Bot::Client.new(TOKEN).api.send_photo(chat_id: 173948014, photo: Faraday::UploadIO.new(File.expand_path("p#{i + 1}.jpg"), 'image/jpeg'))['result']['photo'].first['file_id']
-end

956a720e992937285c2734711f4f4b0eb6acfab1
Aleksandr Sucharev <alx19sucharev@gmail.com>
2023-07-21 12:29:07 +0300

    change reminder message

--- reminder.rb
+++ reminder.rb
@@ -4,7 +4,7 @@ require 'telegram/bot'
 TOKEN = ''
 
 MongoClient.not_reminded.each do |rem|
-  Telegram::Bot::Client.new(TOKEN).api.send_message(chat_id: rem['id'], text: "Напоминаем вам о записи на массаж #{rem['russian_datetime']}")
+  Telegram::Bot::Client.new(TOKEN).api.send_message(chat_id: rem['id'], text: "Напоминаем вам о записи на массаж #{rem['russian_datetime']}. Если планы поменялись, напишите @alicekoala, чтобы отменить запись.")
   MongoClient.set_reminded(rem['unix_timestamp'])
   MyLogger.new('succeded.txt').log("Напоминание о массаже #{rem['russian_datetime']} для #{rem['id']} успешно")
 rescue => e

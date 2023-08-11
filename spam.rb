8a5611a5284f711ce3e8288443d2efb83cd8b86e
Aleksandr Sukharev <aleksandr.sukharev@sbermarket.ru>
2023-08-11 19:47:25 +0300

    adding announcements

---
+++ spam.rb
@@ -0,0 +1,14 @@
+require_relative 'mongo_client'
+require_relative 'my_logger'
+require 'telegram/bot'
+TOKEN = ''
+
+file = File.open('announcement.txt', 'r')
+file_contents = file.read
+file.close
+
+MongoClient.show_users.each do |user|
+  Telegram::Bot::Client.new(TOKEN).api.send_message(chat_id: user['id'], text: file_contents)
+rescue => e
+  MyLogger.new('failed_spam.txt').log_error(e)
+end
